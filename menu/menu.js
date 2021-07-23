15e8408726034e4c36e4d5dfb4912fd6c57ed2c2
Nabuto Ls <87817483+NabutoLss@users.noreply.github.com>
2021-07-23 01:58:32 -0400

    Add files via upload

---
+++ menu/menu.js
@@ -0,0 +1,349 @@
+const menu = () => { 
+	return `
+╭─────────────────
+┃► SEU NOME: ${pushname}       
+┃
+┃
+┃Olá ${pushname} Esta é a lista de comandos do BOT
+┃
+┃➙ *${prefix}Sticker*
+┃(Faz figurinha)
+┃
+┃➙ *${prefix}spam*
+┃(spam para o whatsapp desejado)
+┃
+┃➙ *${prefix}emoji*
+┃(Faz figurinha com emoji)
+┃
+┃➙ *${prefix}bloquear*
+┃(Bloqueia o usuário de usar o bot)
+┃
+┃➙ *${prefix}desbloquear*
+┃(desbloquea o usuário de usar o bot)
+┃
+┃➙ *${prefix}hidetag*
+┃(marca todos do grupo com mensagem)
+┃
+┃➙ *${prefix}play*
+┃(baixa músicas pelo título)
+┃
+┃➙ *${prefix}play2*
+┃(baixa músicas pelo título 2)
+┃
+┃➙ *${prefix}play3*
+┃(baixa músicas pelo título 3)
+┃
+┃➙ *${prefix}play4*
+┃(baixa músicas pelo título 4)
+┃
+┃➙ *${prefix}linkgp*
+┃(link do grupo)
+┃
+┃➙ *${prefix}listaonline*
+┃(lista de membros online)
+┃
+┃➙ *${prefix}infogrupo*
+┃(informações do grupo)
+┃
+┃➙ *${prefix}contar*
+┃(contar segundos)
+┃
+┃➙ *${prefix}tmsticker*
+┃(faz tm com sticker)
+┃
+┃➙ *${prefix}tmvídeo*
+┃(faz tm com vídeo)
+┃
+┃➙ *${prefix}tmgif*
+┃(faz tm com gif
+┃
+┃➙ *${prefix}tmáudio*
+┃(faz transmição com áudio)
+┃
+┃➙ *${prefix}stickernome*
+┃(faz figurinha com seu nome exemplo: /stickernome Nome | nome
+┃
+┃➙ *${prefix}marcargif*
+┃(marcar todos do grupo com gif)
+┃
+┃➙ *${prefix}marcarsticker*
+┃(marca todos do grupo com sticker)
+┃
+┃➙ *${prefix}marcarcontato*
+┃(marca todos do grupo com contato)
+┃
+┃➙ *${prefix}marcartudo*
+┃(marcar imagem, áudio, vídeo, documento e etc)
+┃
+┃➙ *${prefix}wame*
+┃(cria um link do seu contato)
+┃
+┃➙ *${prefix}perfil*
+┃(seu perfil)
+┃
+┃➙ *${prefix}attp*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}attp1*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}attp2*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}attp3*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}attp4*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}attp5*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}attp6*
+┃(faz sticker com texto)
+┃
+┃➙ *${prefix}perfildeanime*
+┃(Manda perfil de animes para vc usa)
+┃
+┃➙ *${prefix}perfilhentai*
+┃(Manda perfil de hentai para vc usa)
+┃
+┃➙ *${prefix}wallpaperanime*
+┃(wallpaper de anime)
+┃
+┃➙ *${prefix}memes*
+┃(Manda memes para vc)
+┃
+┃➙ *${prefix}robô*
+┃(Deixa seu áudio com voz de robo)
+┃
+┃➙ *${prefix}printsite*
+┃(Tira print do site que vc mandou)
+┃
+┃➙ *${prefix}printsite2*
+┃(Tira print do site que vc mandou maneira 2)
+┃
+┃➙ *${prefix}gerarnick*
+┃(Gerar nick)
+┃
+┃➙ *${prefix}banir*
+┃(Banir alguém do grupo)
+┃
+┃➙ *${prefix}mudarfoto*
+┃(mudar foto do grupo
+┃
+┃➙ *${prefix}mudardesc
+*
+┃(mudar descrição do grupo)
+┃
+┃➙ *${prefix}mudarnome
+*
+┃(mudar nome do grupo)
+┃
+┃➙ *${prefix}promover
+*
+┃(pormover alguem do grupo)
+┃
+┃➙ *${prefix}inverter*
+┃(Deixa seu áudio ao contrário)
+┃
+┃➙ *${prefix}reportarbug*
+┃(Caso vc tenha alguma crítica ou algum erro sobre o BOT use este comando para entrar em contato com o dono deste bot)
+┃	
+┃➙ *${prefix}sticker*
+┃(faz figurinha/sticker)
+┃
+┃➙ *${prefix}geraranimes
+*
+┃(gera nomes de animes para assistir)
+┃
+┃➙ *${prefix}baiano*
+┃medir sua porcentagem
+┃
+┃➙ *${prefix}medirpau*
+┃tamanho da sua pingola
+┃
+┃➙ *${prefix}antiexcluir*
+┃(Sistema de antiexcluir)
+┃
+┃➙ *${prefix}hentai
+*
+┃(envia imagens de personagens 2d nuas🌝👍)
+┃
+┃➙ *${prefix}pegarvideo*
+┃(transforma sticker animados em gifs)
+┃
+┃➙ *${prefix}toimg*
+┃(converte stickers normais em imagens)
+┃
+┃➙ *${prefix}pinterest*
+┃(pesquisa por imagens no Pinterest)
+┃
+┃➙ *${prefix}cep*
+┃(acha cidade e estado, bairro pelo cep)
+┃
+┃➙ *${prefix}pegaráudio*
+┃(pegar audio de algum vídeo)
+┃
+┃➙ *${prefix}plaquinha*
+┃(faz uma plaquinha fake bem bosta)
+┃
+┃➙ *${prefix}calculadora*
+┃(calculadora)
+┃
+┃➙ *${prefix}sortear*
+┃(O bot ira marcar 5 pessoas aleatoriamentes do grupo)
+┃
+┃➙ *${prefix}antilinkgeral* 1/0
+┃(bloqueia qualquer tipo de link no seu grupo)
+┃
+┃➙ *${prefix}banir*
+┃(banir alguém do grupo)
+┃
+┃➙ *${prefix}promover*
+┃(dar adm para alguém do grupo)
+┃
+┃➙ *${prefix}rebaixar*
+┃(tirar alguém de adm no grupo)
+┃
+┃➙ *${prefix}grupo* fechar/abrir
+┃(abrir ou fechar o grupo)
+┃
+┃➙ *${prefix}pegaráudio*
+┃(pegar áudio de um vídeo)
+┃
+┃➙ *${prefix}antifake*
+┃(bloqueia qualquer número que não seja +55 de entrar no seu grupo)
+┃
+┃➙ *${prefix}antilinkgeral*
+┃(bloqueia qualquer tipo de link do seu grupo)
+┃
+┃➙ *${prefix}pegarperfil*
+┃(pegar o perfil da pessoa)
+┃
+┃➙ *${prefix}pinterest*
+┃(procura imagens no Pinterest)
+┃
+┃➙ *${prefix}gay*
+┃(mede sua porcentagem de gay)
+┃
+┃➙ *${prefix}frioecalculista*
+┃(mede sua porcentagem de frio)
+┃
+┃➙ *${prefix}corno*
+┃(mede sua porcentagem de corno)
+┃
+┃➙ *${prefix}nazista*
+┃(mede sua porcentagem de nazista)
+┃
+┃➙ *${prefix}punheteiro*
+┃(mede sua porcentagem de punheteiro)
+┃
+┃➙ *${prefix}grossa*
+┃(marque um áudio)
+┃
+┃➙ *${prefix}esquilo
+*
+┃(marque um áudio)
+┃
+┃➙ *${prefix}lento
+*
+┃(marque um áudio)
+┃
+┃➙ *${prefix}grossa*
+┃(marque um áudio)
+┃
+┃➙ *${prefix}naruto*
+┃(Imagens do naruto)
+┃
+┃➙ *${prefix}mikasa*
+┃(Imagens da mikasa)
+┃
+┃➙ *${prefix}eren*
+┃(Imagens do eren)
+┃
+┃➙ *${prefix}titan*
+┃(Imagens de titans)
+┃
+┃➙ *${prefix}levi*
+┃(Imagens do gostoso do levi🥵)
+┃
+┃➙ *${prefix}sakura*
+┃(Imagens da sakura
+┃
+┃➙ *${prefix}hinata*
+┃(Imagens da hinata)
+┃
+┃➙ *${prefix}neji*
+┃(Imagens do neji
+┃
+┃➙ *${prefix}minato*
+┃(Imagens do minato)
+┃
+┃➙ *${prefix}jiraya*
+┃(Imagens do eterno jiraya😓)
+┃
+┃➙ *${prefix}tsunade*
+┃(Imagens da tsunade)
+┃
+┃➙ *${prefix}kiba*
+┃(Imagens do kiba gostosãn)
+┃
+┃➙ *${prefix}boruto*
+┃(Imagens do boruto)
+┃
+┃➙ *${prefix}sarada*
+┃(Imagens da sarada)
+┃
+┃➙ *${prefix}sasuke*
+┃(Imagens do lindo sasuke)
+┃
+┃➙ *${prefix}madara*
+┃(Imagens do madara)
+┃
+┃➙ *${prefix}obito*
+┃(Imagens do obito)
+┃
+┃➙ *${prefix}tanjiro*
+┃(Imagens do tanjiro)
+┃
+┃➙ *${prefix}nezuko*
+┃(Imagens do nezuko)
+┃
+┃➙ *${prefix}luffy*
+┃(Imagens do luffy)
+┃
+┃➙ *${prefix}zoro*
+┃(Imagens do perdido do zoro)
+┃
+┃➙ *${prefix}sanji*
+┃(Imagens do sanji)
+┃
+┃➙ *${prefix}gon*
+┃(imagens gon)
+┃
+┃➙ *${prefix}killua*
+┃(Imagens do killua)
+┃
+┃➙ *${prefix}sagiri*   
+┃(Imagens sagiri)
+┃
+┃➙ *${prefix}rimuru*
+┃(Imagens do rimuru)
+┃
+┃➙ *${prefix}natsu*
+┃(Imagens do natsu)
+┃
+┃➙ *${prefix}genos*
+┃(Imagens do genos)
+┃
+┃➙ *${prefix}saitama*
+┃(Imagens do saitama)
+┃
+┃➙ *${prefix}miku*
+┃(Imagens do miku)
+┗━━━━━━━━━━━━━
+`
+}
+
+exports.menu = menu
